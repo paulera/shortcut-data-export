e680fb29f066e89011ae79a5ae4ce500083a66dc
Paulo Amaral <paulo.amaral@gmail.com>
2022-05-19 00:16:20 +0200

    Refactor configuration into class app\Config
    Moves all the configuration (host, token, api path and workflow states
    mapping into a class in which these values can't be modified during
    runtime.
    
    * getdata.php
      Refactor config calls from `$config['property']` to `Config::property`
      Removed use of `global` variables
    * ./tests/tests/test_basic_info_iteration_176981.sh
      Makes a call to getbasicinfo for the iteration 176981 and compares to
      an expected result ./tests/basic_info_iteration_176981.txt

--- config.example.php
+++ config.example.php
@@ -1,4 +1,4 @@
-<?php
+<?php namespace app;
 
 // create a copy of this file renamed as config.php and set your TOKEN
 
@@ -8,21 +8,26 @@
 // and click "Generate Token". You can redo this process as many
 // times as you want.
 
-$config = [
-    'apikey' => 'YOUR API KEY HERE',
-    'host' => 'https://api.app.shortcut.com',
-    'endpoint' => '/api/v3'
-];
+/**
+ * Constants that setup the application.
+ */
+class Config
+{
+    const apikey = '---> YOUR API KEY HERE <---';
+    const host = 'https://api.app.shortcut.com';
+    const endpoint = '/api/v3';
 
-$workflowStateMap = array(
-    500000003 => 'ToDo',
-    500000004 => 'In Development',
-    500000005 => 'Ready for Review',
-    500000204 => 'Ready for QA',
-    500007062 => 'In QA',
-    500005143 => 'Ready for Merge',
-    500000006 => 'Pending Approval',
-    500000102 => 'Waiting for Deploy',
-    500000025 => 'Deployed Live',
-    500000174 => 'Completed'
-);
+    const workflowStateMap = array(
+        500000003 => 'ToDo',
+        500000004 => 'In Development',
+        500000005 => 'Ready for Review',
+        500000204 => 'Ready for QA',
+        500007062 => 'In QA',
+        500005143 => 'Ready for Merge',
+        500000006 => 'Pending Approval',
+        500000102 => 'Waiting for Deploy',
+        500000025 => 'Deployed Live',
+        500000174 => 'Completed'
+    );
+
+}
