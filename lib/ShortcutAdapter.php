2b68137277b48d777e3204041b1ee8874d1a3dad
Paulo Amaral <paulo.amaral@gmail.com>
2022-05-24 01:11:33 +0200

    Use the iterations andpoint to list stories
    Use a better performing and v3 compliant resource to list
    stories by iteration.
    
    * lib/SHortcutAdapter.php
      function getStoriesByIteration() using the path
      "/iterations/iterationId/stories"
    * tests/basic_info_iteration_176981.txt
      Updated test result after column position change.

--- lib/ShortcutAdapter.php
+++ lib/ShortcutAdapter.php
@@ -9,11 +9,9 @@ class ShortcutAdapter
 {
     public static function getStoriesByIteration($iterationId) {
         error_log("Requesting stories of iteration ${iterationId}", 4);
-        $url = Config::host . Config::endpoint . '/stories/search';
+        $url = Config::host . Config::endpoint . "/iterations/${iterationId}/stories";
         $request = new Request($url);
-        $result = $request->post([
-            'iteration_id' => $iterationId
-        ]);
+        $result = $request->get();
         error_log(count($result) . " stories found", 4);
         return $result;
     }
